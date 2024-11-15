985017552f317e108d286702f256f05e080e4def
eawong1 <eedwardwong@gmail.com>
2024-11-14 23:08:21 -0700

    made api connection

--- FantasyBasketball/League.cs
+++ FantasyBasketball/League.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.VisualBasic;
+
 public class League
 {
     private string? m_leagueId;
@@ -5,16 +10,60 @@ public class League
     private string? m_swid;
     private string? m_espn;
 
+    //$"https://lm-api-reads.fantasy.espn.com/apis/v3/games/fba/seasons/2025/segments/0/leagues/1282650304";
+    private const string BASE_ENDPOINT = $"https://lm-api-reads.fantasy.espn.com/apis/v3/games/fba/seasons";
+
     public League(string? leagueId, string? leagueYear, string? swid, string? espn)
     {
         m_leagueId = leagueId;
         m_leagueYear = leagueYear;
         m_swid = swid;
         m_espn = espn;
+
+        Login();
     }
 
-    public void Login()
+    public async void Login()
     {
+        if(m_leagueId == null || m_leagueYear == null)
+        {
+            throw new Exception("League ID and League Year cannot be empty.");
+        }
+        var url = BASE_ENDPOINT + string.Format("/{0}/segments/0/leagues/{1}", m_leagueYear, m_leagueId);
 
+        var handler = new HttpClientHandler
+        {
+            CookieContainer = new CookieContainer()
+        };
+
+        // Add cookies to the handler's CookieContainer
+        handler.CookieContainer.Add(new Uri(url), new Cookie("swid", m_swid));
+        handler.CookieContainer.Add(new Uri(url), new Cookie("espn_s2", m_espn));
+
+        // Create an HttpClient instance
+        using (HttpClient client = new HttpClient(handler))
+        {
+            try
+            {
+                // Make the GET request
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                // Check if the request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read the response content as a string
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Response Data: " + responseData);
+                }
+                else
+                {
+                    throw new Exception("ResponseData Failed");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
