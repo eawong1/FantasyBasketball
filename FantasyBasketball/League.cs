1a83b80743f4b0366b41726386dad7e9f3fa416c
eawong1 <eedwardwong@gmail.com>
2024-11-15 20:10:21 -0700

    change request query a bit

--- FantasyBasketball/League.cs
+++ FantasyBasketball/League.cs
@@ -1,6 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Avalonia.Markup.Xaml.Templates;
 using Microsoft.VisualBasic;
 
 public class League
@@ -9,6 +14,7 @@ public class League
     private string? m_leagueYear;
     private string? m_swid;
     private string? m_espn;
+    private Dictionary<string, object> m_responseData;
 
     //$"https://lm-api-reads.fantasy.espn.com/apis/v3/games/fba/seasons/2025/segments/0/leagues/1282650304";
     private const string BASE_ENDPOINT = $"https://lm-api-reads.fantasy.espn.com/apis/v3/games/fba/seasons";
@@ -29,7 +35,8 @@ public class League
         {
             throw new Exception("League ID and League Year cannot be empty.");
         }
-        var url = BASE_ENDPOINT + string.Format("/{0}/segments/0/leagues/{1}", m_leagueYear, m_leagueId);
+        // var url = BASE_ENDPOINT + string.Format("/{0}/segments/0/leagues/{1}", m_leagueYear, m_leagueId);
+        var url = BASE_ENDPOINT + $"/{m_leagueYear}/segments/0/leagues/{m_leagueId}?view=mTeam&view=mRoster&view=mMatchup&view=mSettings&view=mStandings";
 
         var handler = new HttpClientHandler
         {
@@ -52,8 +59,14 @@ public class League
                 if (response.IsSuccessStatusCode)
                 {
                     // Read the response content as a string
-                    string responseData = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine("Response Data: " + responseData);
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    // var responseData = await response.Content.ReadFromJsonAsync();
+                    //keys are draftDetail, gameId, id, members, schedule, scoringPeriodId, seasonId, segmentId, settings, status, teams
+                    m_responseData = JsonSerializer.Deserialize<Dictionary<string, object>>(responseData);
+                    
+                    var temp = m_responseData["teams"];
+                    File.AppendAllText("temp.txt", temp.ToString());
+
                 }
                 else
                 {
