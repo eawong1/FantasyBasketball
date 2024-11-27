dc5899e6fc662055c2b88eb6ede61f9ac12da57e
eawong1 <44224189+eawong1@users.noreply.github.com>
2024-11-27 14:53:41 -0700

    made MainWindow bare and moved all login to Login UserControl

--- FantasyBasketball/MainWindow.axaml.cs
+++ FantasyBasketball/MainWindow.axaml.cs
@@ -5,31 +5,10 @@ using Avalonia.Interactivity;
 
 namespace FantasyBasketball;
 
-public partial class MainWindow : UserControl
+public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        InitializeComponent();
+        Content = new Login();
     }
-
-    private void LoginButton_Click(object? sender, RoutedEventArgs e)
-    {
-        // Your login logic here
-        string leagueId = LeagueIdTextBox.Text;
-        string leagueYear = LeagueYearTextBox.Text;
-        string swid = SwidTextBox.Text;
-        string espnS2 = espnTextBox.Text;
-
-        Console.WriteLine("League ID: " + leagueId);
-        Console.WriteLine("League Year: " + leagueYear);
-        Console.WriteLine("SWID: " + swid);
-        Console.WriteLine("ESPN S2: " + espnS2);
-
-        // League league = new League(leagueId, leagueYear, swid, espnS2);
-        if (this.Parent is ContentControl contentControl)
-        {
-            contentControl.Content = new TeamSelect();
-        }
-    }
-
 }
