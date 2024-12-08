1d8f6249d587bce5b1e4f006760c54b8bb79e6ae
eawong1 <44224189+eawong1@users.noreply.github.com>
2024-12-08 12:34:23 -0700

    trying to make function select page

---
+++ FantasyBasketball/FunctionSelect.axaml.cs
@@ -0,0 +1,16 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Utilities;
+
+
+namespace FantasyBasketball;
+
+public partial class FunctionSelect : UserControl
+{
+    public FunctionSelect()
+    {
+        InitializeComponent();
+    }
+
+}
